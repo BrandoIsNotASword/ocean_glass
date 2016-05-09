7a4a8897fc793f30768a201a82a36f79f77c8511
Luis Uicab <uicab2593@gmail.com>
2016-05-09 00:20:03 -0500

    asd

--- common/config/production.php
+++ common/config/production.php
@@ -3,16 +3,16 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=lagrannacion_sistema',
-            'username' => 'lagrannacion',
-            'password' => 'o8yA9@h4',
+            'dsn' => 'mysql:host=localhost;dbname=oceanglass',
+            'username' => 'root',
+            'password' => 'cJrhL7V2',
             'charset' => 'utf8',
         ],
         'db2' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=lagrannacion_imagenes',
-            'username' => 'lagrannacion2',
-            'password' => 'o8yA9@h4',
+            'dsn' => 'mysql:host=localhost;dbname=oceanglass_images',
+            'username' => 'root',
+            'password' => 'cJrhL7V2',
             'charset' => 'utf8',
         ],
         'mailer' => [
@@ -20,14 +20,5 @@ return [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
         ],
-        'urlManager'=>[
-            'showScriptName' => false,
-            'enablePrettyUrl' => true,
-            'rules'=>[
-                '<controller:\w+>/<id:\d+>'=>'<controller>/view',
-                '<controller:\w+>/<action:\w+>/<id:\S+>'=>'<controller>/<action>',
-                '<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
-            ],
-        ]
     ],
 ];
